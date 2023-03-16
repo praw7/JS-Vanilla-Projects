25f405e1265b8759778c6b85e831de358d54d531
Hubert Zawisza <hubert.zawisza90@gmail.com>
2023-03-16 11:26:24 +0100

    update faq

--- faq/FAQ.js
+++ faq/FAQ.js
@@ -1,4 +1,30 @@
 //using selectors inside the element
 // traversing the dom
 
-//SELECTORS
+// SELECTORS
+const questions = document.querySelectorAll(".question");
+
+questions.forEach(function (question) {
+  const btn = question.querySelector(".question-btn");
+
+  btn.addEventListener("click", () => {
+    questions.forEach(function (item) {
+      if (item !== question) {
+        item.classList.remove("show-text");
+      }
+    });
+
+    question.classList.toggle("show-text");
+  });
+});
+
+// // DOM
+// const btns = document.querySelectorAll(".question-btn");
+//
+// btns.forEach((btn) => {
+//   btn.addEventListener("click", (e) => {
+//     const question = e.currentTarget.parentElement.parentElement;
+//
+//     question.classList.toggle("show-text");
+//   });
+// });
